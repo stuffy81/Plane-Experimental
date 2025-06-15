c4e80aa5249ca606aa490713ab7aa3cb473d8af4
Freddy Olivares <olivaresgabriel032@gmail.com>
2025-06-15 10:09:40 -0700

    Second  Commit

--- Scripts/camera_controller.gd
+++ Scripts/camera_controller.gd
@@ -1,11 +1,7 @@
 extends Node3D
 class_name CameraController
 
-#Nodo Padre 
 @onready var player:Player = self.owner
-
-#Interpolacion
-@export var weight_interpolated:float 
 
 func _physics_process(delta: float) -> void:
 	rotation.y = lerpf(rotation.y , player.model_player.rotation.y , delta)   
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,18 +1,10 @@
 extends CharacterBody3D
 class_name Player
 
-@export var speed:float = 8.0
-
-#Rotaciones
-@export var rotacion_speed:float = 0.8
-var target_rotation = Vector2()
-var rot_z = 0.0
-
-#Interpolacion Velocidad
-@export var weight_interpolated:float = 3.0
-
-#Nodos  
 @export var model_player:Node3D 
+@export var speed:float = 15.0
+@export var rotation_speed:float = 0.8
+var target_rotation = Vector3()
 
 func _physics_process(delta: float) -> void:
 	_gravity(delta)
@@ -21,25 +13,25 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _move_player(delta):
-	velocity.x = lerpf(velocity.x , model_player.get_global_transform().basis.z.x * speed , weight_interpolated * delta)
-	velocity.y = lerpf(velocity.y , model_player.get_global_transform().basis.z.y * speed , weight_interpolated * delta)
-	velocity.z = lerpf(velocity.z , model_player.get_global_transform().basis.z.z * speed , weight_interpolated * delta)
+	var direction = model_player.get_global_transform().basis.z * speed 
+	velocity = lerp(velocity , direction , speed * delta)
 
 func _rotating(delta):
-	target_rotation += _get_axis() * rotacion_speed * delta
-	target_rotation.x = clampf(target_rotation.x , -360 , 360)
-	target_rotation.y = clampf(target_rotation.y , -360 , 360)
+
+	if not model_player:
+		return
 
 	if _get_axis().length() > 0:
-		rot_z += -_get_axis().x * rotacion_speed * delta
-		rot_z = clampf(rot_z , deg_to_rad(-30) , deg_to_rad(30))
-	else:
-		rot_z = lerpf(rot_z , 0.0 , weight_interpolated * delta)
+		target_rotation = Vector3(model_player.rotation.x + _get_axis().y  , model_player.rotation.y + _get_axis().x, model_player.rotation.z + -_get_axis().x)
+		model_player.rotation.x = lerp_angle(model_player.rotation.x , target_rotation.x , rotation_speed * delta)
+		model_player.rotation.y = lerp_angle(model_player.rotation.y , target_rotation.y , rotation_speed * delta)
 
-	if model_player:
-		model_player.rotation.y = lerp_angle(model_player.rotation.y , target_rotation.x , weight_interpolated * delta)
-		model_player.rotation.x = lerp_angle(model_player.rotation.x , target_rotation.y , weight_interpolated * delta)
-		model_player.rotation.z = lerp_angle(model_player.rotation.z , rot_z , weight_interpolated * delta)
+		target_rotation.z = clampf(target_rotation.z , deg_to_rad(-60) , deg_to_rad(60))
+
+	else:
+		target_rotation = Vector3()
+
+	model_player.rotation.z = lerp_angle(model_player.rotation.z , target_rotation.z , rotation_speed * delta)
 
 func _gravity(delta):
 	if not is_on_floor():
